1214a07dc3cca5640714e2de182d497350658869
Baransky-Dmitry <baranskiy73@gmail.com>
2021-11-20 20:24:48 +0300

    section-gallery

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
-    // Slider
-    $('.slider__inner, .news__slider-inner').slick({
-        nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
-        prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-        infinite: false,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        cssEase:'linear'
-    });
-    // Select
-    $('input, select').styler();
-    // Mobile menu
-    $('.header__btn-menu').on('click', function(){
-        $('.menu ul').slideToggle();
-    });
+    $('.popup__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
     
 });
